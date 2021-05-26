813a4ffe81d16355374876ec7f0cf095290c4bc0
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2021-05-26 20:11:49 +0200

    .

--- vultr/run.sh
+++ vultr/run.sh
@@ -18,8 +18,7 @@ rm -rf /tmp/*
 > /var/log/php7.4-fpm.log
 > /var/log/user.log
 > /root/.ssh/authorized_keys
-> /etc/cloud/cloud.cfg.d/100-clp.cfg
-echo "manage_etc_hosts: false" > /etc/cloud/cloud.cfg.d/100-clp.cfg
+rm /etc/cloud/cloud.cfg.d/100-clp.cfg
 sed -i 's/^CLOUDPANEL_URL=.*/CLOUDPANEL_URL="https:\/\/$(curl -s http:\/\/169.254.169.254\/v1\/interfaces\/0\/ipv4\/address):8443"/' /etc/update-motd.d/10-cloudpanel
 cloud-init clean
 
