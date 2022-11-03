a13ec10a5cc9a25facb2c041bcb9d24d788700fd
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2022-11-03 19:32:36 +0100

    .

--- do/cleanup.sh
+++ do/cleanup.sh
@@ -23,7 +23,6 @@
 > /var/log/cloud-init.log
 > /var/log/unattended-upgrades/unattended-upgrades-shutdown.log
 > /var/log/unattended-upgrades
->
 > /var/log/kern.log
 > /var/log/ubuntu-advantage-timer.log
 > /var/log/ufw.log
